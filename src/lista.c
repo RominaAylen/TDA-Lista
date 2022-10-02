4f522705c45a21b41f5dcff697f93b88940ae747
RominaAylen <ragimenez@fi.uba.ar>
2022-10-02 10:03:42 -0300

    more fixes

--- src/lista.c
+++ src/lista.c
@@ -7,7 +7,6 @@ const char NO_INSERTADO = -1;
 
 lista_t *lista_crear()
 {
-
 	return malloc(sizeof(lista_t));
 }
 
@@ -15,25 +14,26 @@ lista_t *lista_insertar(lista_t *lista, void *elemento)
 {
 	if (!lista)
 		return NULL;
-	struct nodo* nuevo_nodo = malloc(sizeof(struct nodo));
-	
-	if(!nuevo_nodo)
+	struct nodo *nuevo_nodo = malloc(sizeof(struct nodo));
+
+	if (!nuevo_nodo)
 		return NULL;
-	
+
 	nuevo_nodo->siguiente = NULL;
 	nuevo_nodo->elemento = elemento;
-	
-	struct nodo* actual_nodo = lista->nodo_inicio;
-	
-	if(!actual_nodo){
+
+	struct nodo *actual_nodo = lista->nodo_inicio;
+
+	if (!actual_nodo)
+	{
 		lista->nodo_inicio = nuevo_nodo;
 		return lista;
 	}
 
 	while (actual_nodo->siguiente)
 	{
-		actual_nodo->siguiente = nuevo_nodo; 
-	return lista;
+		actual_nodo->siguiente = nuevo_nodo;
+		return lista;
 	}
 }
 
@@ -66,38 +66,27 @@ void *lista_buscar_elemento(lista_t *lista, int (*comparador)(void *, void *),
 
 void *lista_primero(lista_t *lista)
 {
-	return NULL;
+	return lista->nodo_inicio;
 }
 
 void *lista_ultimo(lista_t *lista)
 {
-	return NULL;
+	return lista->nodo_fin;
 }
 
 bool lista_vacia(lista_t *lista)
 {
-	return false;
+	return lista || lista->nodo_inicio != NULL;
 }
 
 size_t lista_tamanio(lista_t *lista)
 {
-	int contador = 0;
-	
-	if(!lista || lista->nodo_inicio == NULL)
-		return VACIA;
-
-	struct nodo* actual_nodo = lista->nodo_inicio;
-	while (actual_nodo)
-	{
-		actual_nodo = actual_nodo->siguiente;
-		contador++;
-	}
-	
-	return contador;
+		return lista->cantidad;
 }
 
 void lista_destruir(lista_t *lista)
 {
+ 	free(lista);
 }
 
 void lista_destruir_todo(lista_t *lista, void (*funcion)(void *))
@@ -124,15 +113,15 @@ bool lista_iterador_tiene_siguiente(lista_iterador_t *iterador)
 
 bool lista_iterador_avanzar(lista_iterador_t *iterador)
 {
-	return(iterador->corriente == iterador->corriente->siguiente);
+	return (iterador->corriente == iterador->corriente->siguiente);
 }
 
 void *lista_iterador_elemento_actual(lista_iterador_t *iterador)
 {
-	if(iterador->corriente)
-	return iterador->corriente = iterador->corriente->elemento;
-	else 
-	return NULL;
+	if (iterador->corriente)
+		return iterador->corriente = iterador->corriente->elemento;
+	else
+		return NULL;
 }
 
 void lista_iterador_destruir(lista_iterador_t *iterador)
